1935e00c1e96b8fc0b6e996a59f3121f04c546d4
Selin Kaseler (student) <2880864K@student.gla.ac.uk>
2025-02-13 18:31:19 +0000

    chapter 7 completed test pass

--- populate_rango.py
+++ populate_rango.py
@@ -34,10 +34,12 @@ def populate():
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'])  
+
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
+            
 def add_page(cat, title, url, views=31):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
--- rango/admin.py
+++ rango/admin.py
@@ -4,3 +4,4 @@ from rango.models import Category, Page
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
